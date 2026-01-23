bef45fae175a6290391cbc7c7cb72944b287a874
matej2005 <68588948+matej2005@users.noreply.github.com>
2026-01-23 16:48:24 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,27 @@
-BUILD_FILE="./build/MARS-engineers-boards-test.zip"
-rm -f $BUILD_FILE
+#!/bin/sh
+set -e
 
-zip -rq $BUILD_FILE hardware/robotrix
+VENDOR="MARS-Engineers"
+ARCH="robotrix"
+VERSION="1.0.0"
+REPO_URL="https://github.com/MARS-Engineers/arduino-boards"
+JSON_OUT="package_MARS-engineers_boards_index.json"
+ZIP_NAME="${VENDOR}-boards-${VERSION}.zip"
+RELEASE_URL="${REPO_URL}/releases/download/${VERSION}/${ZIP_NAME}"
+ZIP_PATH="build/${ZIP_NAME}"
+rm -f "$ZIP_PATH"
 
-SIZE=$(stat -c %s $BUILD_FILE)
+zip -rq $ZIP_PATH hardware/robotrix
 
-echo "Size: " $SIZE
+SIZE=$(stat -c %s "$ZIP_PATH")
+SHA256=$(sha256sum "$ZIP_PATH" | awk '{print $1}')
+
+echo "Size: " $SIZE "sum: " $SHA256
+
+sed \
+  -e "s/__VERSION__/$VERSION/g" \
+  -e "s#__URL__#$RELEASE_URL#g" \
+  -e "s/__ZIP__/$ZIP_NAME/g" \
+  -e "s/__SHA256__/$SHA256/g" \
+  -e "s/__SIZE__/$SIZE/g" \
+  package_index.template.json > "$JSON_OUT"
