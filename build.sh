c8e3733669d6fae66c28fbf4978f614b7c95dfa3
matej2005 <68588948+matej2005@users.noreply.github.com>
2026-01-23 16:33:02 +0100

    .

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 BUILD_FILE="./build/MARS-engineers-boards-test.zip"
 rm -f $BUILD_FILE
-zip -rq $BUILD_FILE hardware/rp2350/
+
+zip -rq $BUILD_FILE hardware/robotrix
 
 SIZE=$(stat -c %s $BUILD_FILE)
 
