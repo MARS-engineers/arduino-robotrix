e285ca306aa7733fe81c35bb1ef9127af79b633b
matej2005 <68588948+matej2005@users.noreply.github.com>
2026-01-30 18:11:44 +0100

    Update build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 VENDOR="MARS-Engineers"
-VERSION="1.1.0"
+VERSION="1.1.1"
 REPO_URL="https://github.com/MARS-Engineers/arduino-boards"
 
 JSON_OUT="build/ package_MARS-engineers_boards_index.json "
@@ -36,24 +36,26 @@ sed \
 
 
 # Get previous release name
-curl --silent https://api.github.com/repos/MARS-engineers/arduino-boards/releases > build/temp/releases.json
+#curl --silent https://api.github.com/repos/MARS-engineers/arduino-boards/releases > build/temp/releases.json
 # Previous final release (prerelease == false)
-prev_release=$(jq -r '. | map(select(.draft == false and .prerelease == false)) | sort_by(.created_at | - fromdateiso8601) | .[0].tag_name' build/temp/releases.json)
+#prev_release=$(jq -r '. | map(select(.draft == false and .prerelease == false)) | sort_by(.created_at | - fromdateiso8601) | .[0].tag_name' build/temp/releases.json)
 # Previous release (possibly a pre-release)
-prev_any_release=$(jq -r '. | map(select(.draft == false)) | sort_by(.created_at | - fromdateiso8601)  | .[0].tag_name' build/temp/releases.json)
+#prev_any_release=$(jq -r '. | map(select(.draft == false)) | sort_by(.created_at | - fromdateiso8601)  | .[0].tag_name' build/temp/releases.json)
 # Previous pre-release
-prev_pre_release=$(jq -r '. | map(select(.draft == false and .prerelease == true)) | sort_by(.created_at | - fromdateiso8601)  | .[0].tag_name' build/temp/releases.json)
+#prev_pre_release=$(jq -r '. | map(select(.draft == false and .prerelease == true)) | sort_by(.created_at | - fromdateiso8601)  | .[0].tag_name' build/temp/releases.json)
 
-echo "Previous release: $prev_release"
-echo "Previous (pre-?)release: $prev_any_release"
-echo "Previous pre-release: $prev_pre_release"
+#echo "Previous release: $prev_release"
+#echo "Previous (pre-?)release: $prev_any_release"
+#echo "Previous pre-release: $prev_pre_release"
 
-base_ver=$prev_pre_release
+#base_ver=$prev_pre_release
 
 
 # Download previous release
-echo "Downloading base package: $base_ver"
-curl -L -o $old_json "https://github.com/MARS-engineers/arduino-boards/releases/download/${base_ver}/package_MARS-engineers_index-${base_ver}.json"
+#echo "Downloading base package: $base_ver"
+#curl -L -o $old_json "https://github.com/MARS-engineers/arduino-boards/releases/download/${base_ver}/package_MARS-engineers_index-${base_ver}.json"
+curl -L -o $old_json "https://github.com/MARS-engineers/arduino-boards/releases/download/global/package_MARS-engineers_boards_index.json"
+
 
 python3 tools/merge_packages.py $new_json $old_json > $JSON_OUT
 
