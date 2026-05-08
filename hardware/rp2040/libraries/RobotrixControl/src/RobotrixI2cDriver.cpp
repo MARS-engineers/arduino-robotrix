9995cb57970686f1344155c7dba4c61d9cc3e9a0
matej2005 <68588948+matej2005@users.noreply.github.com>
2026-05-08 19:51:38 +0200

    setup f, onReceive f, debug

--- hardware/rp2040/libraries/RobotrixControl/src/RobotrixI2cDriver.cpp
+++ hardware/rp2040/libraries/RobotrixControl/src/RobotrixI2cDriver.cpp
@@ -1,27 +1,47 @@
 #include "RobotrixI2cDriver.h"
 #include "RobotrixCommand.h"
 #include <Wire.h>
+#include <cstddef>
+#include <cstdint>
 
 I2cCommandBus I2cDriver;
-I2cCommandBus* I2cCommandBus::_instance = nullptr;
+I2cCommandBus *I2cCommandBus::_instance = nullptr;
 
 void I2cCommandBus::setup(uint8_t address) {
   _instance = this;
-  Wire.begin(address);
-  Wire.onReceive(onReceiveStatic);
+  CommandWire.begin(address);
+  CommandWire.onReceive(onReceiveStatic);
+}
+
+void I2cCommandBus::send(uint8_t address, uint8_t *data, size_t size) {
+  if (_instance) {
+    CommandWire.writeAsync(address, data, size);
+  }
 }
 
 void I2cCommandBus::onReceiveStatic(int len) {
-  if (_instance)
+
+  if (_instance) {
     _instance->onReceive(len);
+  }
 }
+
+void I2cCommandBus::debugOn(bool debug) { _DebugOn = debug; }
 
 void I2cCommandBus::onReceive(uint8_t len) {
   uint8_t buf[16];
   uint8_t i = 0;
 
-  while (Wire.available() && i < sizeof(buf))
-    buf[i++] = Wire.read();
+  while (CommandWire.available() && i < sizeof(buf))
+    buf[i++] = CommandWire.read();
 
   Remote.dispatch(buf, i);
+
+  if (_DebugOn) {
+    Serial.printf("bytes: %i, data: ", len);
+    for (byte i = 0; i <= len; i++) {
+      Serial.printf(" %02x", buf[i]);
+    }
+    Serial.println("");
+  }
 }
--- hardware/rp2040/libraries/RobotrixControl/src/RobotrixI2cDriver.h
+++ hardware/rp2040/libraries/RobotrixControl/src/RobotrixI2cDriver.h
@@ -1,15 +1,19 @@
 #pragma once
 #include <Arduino.h>
+#include <cstdint>
 
 class I2cCommandBus {
 public:
-  void setup(uint8_t address);
 
+  void setup(uint8_t address = 0x55);
+  void send(uint8_t address, uint8_t *data, size_t size);
+  void debugOn(bool debug = true);
 private:
   static void onReceiveStatic(int len);
   void onReceive(uint8_t len);
 
-  static I2cCommandBus* _instance;
+  static I2cCommandBus *_instance;
+  bool _DebugOn = false;
 };
 
 extern I2cCommandBus I2cDriver;
