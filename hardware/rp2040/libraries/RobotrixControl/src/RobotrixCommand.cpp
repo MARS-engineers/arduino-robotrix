2ce44142b605e8bcaf56e66dea603e8921a4f2f7
matej2005 <68588948+matej2005@users.noreply.github.com>
2026-05-08 19:50:16 +0200

    helper f, onReceive f, callback data simplification

--- hardware/rp2040/libraries/RobotrixControl/src/RobotrixCommand.cpp
+++ hardware/rp2040/libraries/RobotrixControl/src/RobotrixCommand.cpp
@@ -1,4 +1,6 @@
 #include "./RobotrixCommand.h"
+#include "Arduino.h"
+#include <cstdint>
 
 CommandRouter Remote;
 
@@ -10,16 +12,53 @@ bool CommandRouter::registerCommand(uint8_t cmd, RemoteCallback cb) {
   return true;
 }
 
-void CommandRouter::dispatch(const uint8_t* data, uint8_t len) {
-  if (len < 2) return;
+bool CommandRouter::registerOnReceive(RemoteCallback cb) {
+  everyCmdCb = cb;
+  return true;
+}
 
-  uint8_t cmd    = data[0];
+void CommandRouter::dispatch(const uint8_t *data, uint8_t len) {
+  if (len < 2)
+    return;
+  uint8_t cmd = data[0];
   uint8_t subcmd = data[1];
+
+  everyCmdCb(data, len);
+
+  /*  Serial.printf("I2C Cmd: 0x%02x, SubCmd: 0x%02x, Arg1: 0x%02x, Arg2:
+    0x%02x, " "Arg3: 0x%02x", data[0], data[1], data[2], data[3], data[4]);
+
+    Serial.printf("bytes: %i, data: ", len);
+    for (uint8_t i = 0; i <= len; i++) {
+      Serial.printf(" %02x", data[i]);
+    }
+    Serial.println("");
+  */
 
   for (uint8_t i = 0; i < _count; i++) {
     if (_table[i].cmd == cmd) {
-      _table[i].cb(subcmd, data + 2, len - 2);
+      _table[i].cb(data, len);
       return;
     }
   }
+}
+
+CommandRouter::dataFormat CommandRouter::convertData(const uint8_t *data,
+                                                     uint8_t len) {
+  CommandRouter::dataFormat df;
+  if (len == 0)
+    return df;
+
+  uint8_t dfp = 0;
+
+  df.command = data[dfp++];
+
+  if (len >= 1)
+    df.subCommand = data[dfp++];
+
+  for (uint8_t i = 0; i < len - 2; i++) {
+    df.arg[i] = data[dfp++];
+  }
+
+  return df;
 }
--- hardware/rp2040/libraries/RobotrixControl/src/RobotrixCommand.h
+++ hardware/rp2040/libraries/RobotrixControl/src/RobotrixCommand.h
@@ -1,21 +1,31 @@
 #pragma once
+#include <cstdint>
 #include <stdint.h>
 
-using RemoteCallback = void (*)(uint8_t subcmd,
-                                const uint8_t* args,
-                                uint8_t arg_len);
+using RemoteCallback = void (*)(const uint8_t *data,
+                                uint8_t data_len);
 
 class CommandRouter {
 public:
   bool registerCommand(uint8_t cmd, RemoteCallback cb);
+  bool registerOnReceive(RemoteCallback cb);
 
-  void dispatch(const uint8_t* data, uint8_t len);
+  void dispatch(const uint8_t *data, uint8_t len);
+
+  struct dataFormat {
+    uint8_t command;
+    uint8_t subCommand;
+    uint8_t arg[4];
+  };
+
+  dataFormat convertData(const uint8_t *data, uint8_t len);
 
 private:
   struct Entry {
     uint8_t cmd;
     RemoteCallback cb;
   };
+  RemoteCallback everyCmdCb;
 
   static constexpr uint8_t MAX_CMDS = 16;
   Entry _table[MAX_CMDS];
